b05a7932ddc87f7b8ce484b50613492d0bb2c074
Anthony Rawlins <s3392244@student.rmit.edu.au>
2016-07-21 19:30:53 +1000

    Changed credentials
    Changed credentials and callback url to match our app

--- auth0-jquery-sample/auth0-variables.js
+++ auth0-jquery-sample/auth0-variables.js
@@ -1,3 +1,3 @@
-var AUTH0_CLIENT_ID='ibHLd5aHhBKPpZ8FCNaPM5IfimgN7WVJ'; 
+var AUTH0_CLIENT_ID='n8TCPh8Hpng0wF1CMKxjjzPHYOpbq4yB'; 
 var AUTH0_DOMAIN='s3392244.auth0.com'; 
-var AUTH0_CALLBACK_URL=location.href;
+var AUTH0_CALLBACK_URL='http://teamblue.xyz:8079/index.html';
